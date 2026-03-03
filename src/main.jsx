4420fbd699b9b75a115d5c4acef223f925949beb
silvercute20 <c1878-23@itmlyceumalabang.onmicrosoft.com>
2026-03-03 22:34:26 +0800

    Import bootstrap in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
